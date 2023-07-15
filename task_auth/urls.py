67e71944b8ee1a8b755f363cf76a0e2a65cd0539
amaich <amaev773@mail.ru>
2023-07-15 23:41:34 +0300

    registration added

--- task_auth/urls.py
+++ task_auth/urls.py
@@ -6,7 +6,9 @@ app_name = 'task_auth'
 urlpatterns = [
     path('login/', UserLogin.as_view(), name='login'),
     path('logout/', user_logout, name='logout'),
-    path('check/', is_auth_check, name='auth_check')
+    path('registration/', UserRegistration.as_view(), name='registration'),
+    path('check/', is_auth_check, name='auth_check'),
+
 
 
 ]
--- task_auth/views.py
+++ task_auth/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
@@ -23,6 +24,20 @@ class UserLogin(View):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('task_app:task_list', kwargs={}))
+
+
+class UserRegistration(View):
+    def get(self, request):
+        return render(request, 'task_auth/user_registration.html', {})
+
+    def post(self, request):
+        user = User.objects.create_user(username = request.POST["username"],
+                                        password = request.POST["password"])
+        user.save()
+        if user is not None:
+            return HttpResponseRedirect(reverse('task_auth:login'), kwargs={})
+        else:
+            return HttpResponse('nea')
 
 
 def is_auth_check(request):
